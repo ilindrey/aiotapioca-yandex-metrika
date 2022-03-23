f9ffe0410abf0de365e47719fd31a8a494a2fac8
ilindrey <scronullik@fastmail.com>
2022-03-23 18:29:24 +1100

    new release

--- aiotapioca_yandex_metrika/__init__.py
+++ aiotapioca_yandex_metrika/__init__.py
@@ -1,6 +1,6 @@
-__author__ = "Pavel Maksimov"
-__email__ = "vur21@ya.ru"
-__version__ = "2022.03.09"
+__author__ = "Pavel Maksimov, Andrey Ilin"
+__email__ = "andreyilin@fastmail.com"
+__version__ = "2022.03.26"
 
 from .resource_mapping import *
 from .yandex_metrika import (
